29e35094b3cf8e41562933bf65ee4603fe7a0460
Carol.NI <haha54carol@gmail.com>
2017-10-17 23:41:00 +0800

    refactor

--- web/src/views/pages/tasks/tasks-page.js
+++ web/src/views/pages/tasks/tasks-page.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import { List } from 'immutable';
+import React, {Component} from 'react';
+import {List} from 'immutable';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
+import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
 
-import { getNotification, notificationActions } from 'src/notification';
-import { getTaskFilter, getVisibleTasks, tasksActions } from 'src/tasks';
+import {getNotification, notificationActions} from 'src/notification';
+import {getTaskFilter, getVisibleTasks, tasksActions} from 'src/tasks';
 import Notification from '../../components/notification';
 import TaskFilters from '../../components/task-filters';
 import TaskForm from '../../components/task-form';
 import TaskList from '../../components/task-list';
-
 
 export class TasksPage extends Component {
   static propTypes = {
@@ -22,29 +21,35 @@ export class TasksPage extends Component {
     location: PropTypes.object.isRequired,
     notification: PropTypes.object.isRequired,
     removeTask: PropTypes.func.isRequired,
-    tasks: PropTypes.instanceOf(List).isRequired,
+    tasks: PropTypes
+      .instanceOf(List)
+      .isRequired,
     undeleteTask: PropTypes.func.isRequired,
     unloadTasks: PropTypes.func.isRequired,
     updateTask: PropTypes.func.isRequired
   };
 
   componentWillMount() {
-    this.props.loadTasks();
-    this.props.filterTasks(
-      this.getFilterParam(this.props.location.search)
-    );
+    this
+      .props
+      .loadTasks();
+    this
+      .props
+      .filterTasks(this.getFilterParam(this.props.location.search));
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.location.search !== this.props.location.search) {
-      this.props.filterTasks(
-        this.getFilterParam(nextProps.location.search)
-      );
+      this
+        .props
+        .filterTasks(this.getFilterParam(nextProps.location.search));
     }
   }
 
   componentWillUnmount() {
-    this.props.unloadTasks();
+    this
+      .props
+      .unloadTasks();
   }
 
   getFilterParam(search) {
@@ -53,63 +58,43 @@ export class TasksPage extends Component {
   }
 
   renderNotification() {
-    const { notification } = this.props;
-    return (
-      <Notification
-        action={this.props.undeleteTask}
-        actionLabel={notification.actionLabel}
-        dismiss={this.props.dismissNotification}
-        display={notification.display}
-        message={notification.message}
-      />
-    );
+    const {notification} = this.props;
+    return (<Notification
+      action={this.props.undeleteTask}
+      actionLabel={notification.actionLabel}
+      dismiss={this.props.dismissNotification}
+      display={notification.display}
+      message={notification.message}/>);
   }
 
   render() {
     return (
       <div className="g-row">
         <div className="g-col">
-          <TaskForm handleSubmit={this.props.createTask} />
+          <TaskForm handleSubmit={this.props.createTask}/>
         </div>
 
         <div className="g-col">
-          <TaskFilters filter={this.props.filterType} />
+          <TaskFilters filter={this.props.filterType}/>
           <TaskList
             removeTask={this.props.removeTask}
             tasks={this.props.tasks}
-            updateTask={this.props.updateTask}
-          />
+            updateTask={this.props.updateTask}/>
         </div>
 
-        {this.props.notification.display ? this.renderNotification() : null}
+        {this.props.notification.display
+          ? this.renderNotification()
+          : null}
       </div>
     );
   }
 }
 
+// =====================================  CONNECT
+// -------------------------------------
 
-//=====================================
-//  CONNECT
-//-------------------------------------
+const mapStateToProps = createSelector(getNotification, getTaskFilter, getVisibleTasks, (notification, filterType, tasks) => ({notification, filterType, tasks}));
 
-const mapStateToProps = createSelector(
-  getNotification,
-  getTaskFilter,
-  getVisibleTasks,
-  (notification, filterType, tasks) => ({
-    notification,
-    filterType,
-    tasks
-  })
-);
+const mapDispatchToProps = Object.assign({}, tasksActions, notificationActions);
 
-const mapDispatchToProps = Object.assign(
-  {},
-  tasksActions,
-  notificationActions
-);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TasksPage);
+export default connect(mapStateToProps, mapDispatchToProps)(TasksPage);
